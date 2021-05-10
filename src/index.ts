00a710742e524bd62cb57bcb9bdb9dd82b0a08da
Mike Elsmore <mike@elsmore.me>
2021-05-10 14:31:20 +0100

    Complete refactor to simplify life

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,20 @@
 import express from 'express'
 import logger from './logger'
-import ObjectFormat from './object-format'
-import { v4 as uuid } from 'uuid'
+import Optic from './optic'
 
 interface Options {
     capture?: false,
+    dev?: false,
 }
 
 export default (options: Options) => {
   logger.log('Optic middleware loaded')
   return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-      const logHTTP = options.capture || process.env.OPTIC_CAPTURE || false
-      if (logHTTP) {
-        logger.log('Optic logging to terminal')
-        const { body, headers, path, query, method, hostname } = req
-        const { statusCode } = res
-        const responseHeaders = res.getHeaders()
-        console.log(JSON.stringify({
-          uuid: uuid(),
-          request: {
-            host: hostname,
-            method: method,
-            path: path,
-            query:  ObjectFormat(query),
-            headers: ObjectFormat(headers),
-            body: {
-              contentType: headers['content-type'],
-              value: ObjectFormat(body),
-            }
-          },
-          response: {
-            statusCode: statusCode,
-            headers: ObjectFormat(responseHeaders),
-            body: {
-              contentType: 'application/json',
-              value: ObjectFormat(''),
-            }
-          },
-          tags: []
-        }))
-      }
+      const optic = new Optic(options)
+      res.on('close', () => {
+        optic.captureHttpRequest(req, res)
+      })
       next()
     } catch (error) {
       logger.error(error)
