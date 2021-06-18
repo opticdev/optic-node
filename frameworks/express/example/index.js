c8ace12404040a01a6a88d5d71a43dca21e747e1
Mike Elsmore <mike@elsmore.me>
2021-06-18 16:24:11 +0100

    :art: Use middleware rather than handroll the proxy

--- frameworks/express/example/index.js
+++ frameworks/express/example/index.js
@@ -16,42 +16,6 @@ app.use(optic({
 
 app.use('/', createProxyMiddleware({ target: 'https://httpbin.org', changeOrigin: true }));
 
-// app.use((oreq, ores) => {
-//   const options = {
-//     host: 'httpbin.org',
-//     path: oreq.path,
-//     method: oreq.method,
-//     headers: oreq.headers,
-//   };
-
-//   const creq = http
-//     .request(options, pres => {
-//       pres.setEncoding('utf8');
-//       ores.writeHead(pres.statusCode);
-//       pres.on('data', chunk => {
-//         ores.write(chunk);
-//       });
-//       pres.on('close', () => {
-//         ores.end();
-//       });
-//       pres.on('end', () => {
-//         ores.end();
-//       });
-//     })
-//     .on('error', e => {
-//       console.log(e);
-//       try {
-//         ores.writeHead(500);
-//         ores.write(e.message);
-//       } catch (e) {
-//         console.log(e)
-//       }
-//       ores.end();
-//     });
-
-//   creq.end();
-// });
-
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
